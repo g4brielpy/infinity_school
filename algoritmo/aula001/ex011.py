1bf8f6452421205754bd1ee0158060ea943f62ff
gabrielyur1 <141740769+gabrielyur1@users.noreply.github.com>
2023-11-20 15:11:04 -0300

    ex: 011, 012

---
+++ algoritmo/aula001/ex011.py
@@ -0,0 +1,5 @@
+x = input("Escolha um número: ")
+y = input("Escolha um número: ")
+soma = int(x) // int(y)
+
+print(f"A parte inteira da divisão de {x} e {y} é igual a {soma}")
---
+++ algoritmo/aula001/ex012.py
@@ -0,0 +1,4 @@
+x = input("Escolha um número: ")
+soma = int(x)**2
+
+print(f"O quadrado de {x} é {soma}")
