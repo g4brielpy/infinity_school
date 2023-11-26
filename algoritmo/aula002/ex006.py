ec6082d536b43bed4e98a6b7817272abbb889892
Gabriel Iuri <141740769+gabrielyur1@users.noreply.github.com>
2023-11-26 12:37:00 -0300

    Login

--- algoritmo/aula002/ex006.py
+++ algoritmo/aula002/ex006.py
@@ -4,9 +4,9 @@ senha = "123"
 login_user = input("Login: ")
 login_senha = input("Senha: ")
 
-# if user == login_user and senha == login_senha :
-#     print ("Você entrou")
-# elif :
-#     print ("ERRO")
+if user == login_user and senha == login_senha :
+    print ("Entrou")
+else :
+    print ("ERRO")
 
-print(user == login_user and senha == login_senha)
+# print(user == login_user and senha == login_senha)
