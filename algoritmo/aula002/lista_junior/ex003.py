17971b5c54d930d17593075d2eb91df99ed75e58
Gabriel Iuri <141740769+gabrielyur1@users.noreply.github.com>
2023-11-26 13:22:01 -0300

    Lista Junior - ex003, 004

--- algoritmo/aula002/lista_junior/ex003.py
+++ algoritmo/aula002/lista_junior/ex003.py
@@ -1,7 +1,11 @@
-x = input("Informe o primeiro número: ")
-y = input("Informe o segundo número: ")
+x = int(input("Informe o primeiro número: "))
+y = int(input("Informe o segundo número: "))
 
 if x > y :
     print (f"O número {x} é maior que {y}")
+
+elif x == y :
+    print ("Os números são iguais")
+
 else :
     print (f"O número {y} é maior que {x}")
--- algoritmo/aula002/lista_junior/ex004.py
+++ algoritmo/aula002/lista_junior/ex004.py
@@ -1,10 +1,15 @@
-x = input("Informe o primeiro número: ")
-y = input("Informe o segundo número: ")
-z = input("Informe o terceiro número: ")
+x = int(input("Informe o primeiro número: "))
+y = int(input("Informe o segundo número: "))
+z = int(input("Informe o terceiro número: "))
 
 if x > y :
     print(f"O número {x} é o maior que {y} e {z}")
+
 if y > z :
     print(f"O número {y} é maior que {x} e {z}")
+
+elif x == y and y == z and z == x :
+    print("Os números são iguai")
+
 else :
     print(f"O número {z} é maior que {x} e {y}")
