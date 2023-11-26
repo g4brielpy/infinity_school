afa165a269d9af4076667c81e6dcbb31e8fb973f
Gabriel Iuri <141740769+gabrielyur1@users.noreply.github.com>
2023-11-26 12:36:49 -0300

    Lista Junior - ex001, 002, 003

---
+++ algoritmo/aula002/lista_junior/ex001.py
@@ -0,0 +1,10 @@
+x = 24
+y = "teste"
+z = 456,3
+a, b = True, 45
+
+print (type(x))
+print (type(y))
+print (type(z))
+print (type(a))
+print (type(b))
---
+++ algoritmo/aula002/lista_junior/ex003.py
@@ -0,0 +1,7 @@
+x = input("Informe o primeiro número: ")
+y = input("Informe o segundo número: ")
+
+if x > y :
+    print (f"O número {x} é maior que {y}")
+else :
+    print (f"O número {y} é maior que {x}")
