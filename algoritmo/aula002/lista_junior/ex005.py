c0c8516c91e9a236532f558119072d0aa455a2d8
Gabriel Iuri <141740769+gabrielyur1@users.noreply.github.com>
2023-11-26 12:52:43 -0300

    Lista Junior - ex005

---
+++ algoritmo/aula002/lista_junior/ex005.py
@@ -0,0 +1,10 @@
+user = "mario"
+senha = "123"
+
+user_login = input("Informe o usuário: ")
+senha_login = input("Informe a senha: ")
+
+if (user == user_login) and (senha == senha_login):
+    print ("Entrou")
+else:
+    print("ERRO")
