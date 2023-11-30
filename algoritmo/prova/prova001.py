da73db6b1ab93dd40d10a39f0ae345ed19c42c9b
gabrielyur1 <gabrielsantos031bh@gmail.com>
2023-11-30 10:20:25 -0300

    Create prova001.py

---
+++ algoritmo/prova/prova001.py
@@ -0,0 +1,9 @@
+# principais tipagens em Python
+nome = 'Gabriel' # string
+idade = 17 # int
+altura = 1.70 # float
+ativo = True # bool
+
+# solicitação de dado e imprimir na tela
+Name = str(input('Qual o seu nome: '))
+print(f'Olá {Name}, sejá bem vindo!')
