ae508c3191feed65457e97619f86c8f68852dac4
Gabriel Iuri <141740769+gabrielyur1@users.noreply.github.com>
2023-12-02 20:59:59 -0300

    Delete ex007.py

--- algoritmo/aula002/ex007.py
+++
@@ -1,6 +0,0 @@
-palavra = str(input('Digite uma palavra: '))
-
-if 'a' in palavra or 'A' in palavra:
-    print(f'Na palavra {palavra} tem letra A')
-else:
-    print(f'Na palavra {palavra} não tem letra A')
