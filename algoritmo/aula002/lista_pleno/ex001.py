281a8ee98abb02f6f0414c62086986cc20ed49a4
Gabriel Iuri <141740769+gabrielyur1@users.noreply.github.com>
2023-12-02 21:18:52 -0300

    Update ex001.py

---
+++ algoritmo/aula002/lista_pleno/ex001.py
@@ -0,0 +1,8 @@
+numb = int(input('Informe um númeor: '))
+
+if (numb > 0):
+    print(f'O número {numb} é positivo')
+elif (numb < 0):
+    print(f'O número {numb} é negativo')
+else:
+    print('Número inválido')
