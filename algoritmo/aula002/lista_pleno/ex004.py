3f57b3cb7111e848a213c8da1e36b2dc92d38458
gabrielyur1 <141740769+gabrielyur1@users.noreply.github.com>
2023-12-03 10:26:45 -0300

    Create ex004.py

---
+++ algoritmo/aula002/lista_pleno/ex004.py
@@ -0,0 +1,10 @@
+numb_1 = float(input('Digite o primeiro número: '))
+numb_2 = float(input('Digite o segundo número: '))
+numb_3 = float(input('Digite o terceiro número: '))
+
+if (numb_1 > numb_2):
+    print(f'Númeor {numb_1} é o maior')
+elif (numb_2 > numb_3):
+    print(f'Número {numb_2} é o maior')
+else:
+    print(f'Número {numb_3} é o maior')
