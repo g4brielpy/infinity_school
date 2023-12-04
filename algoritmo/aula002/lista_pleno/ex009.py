947b6a4c6da5731287386a89880a12c8c00c6696
Gabriel Iuri <141740769+gabrielyur1@users.noreply.github.com>
2023-12-04 20:51:22 -0300

    Create ex009.py

---
+++ algoritmo/aula002/lista_pleno/ex009.py
@@ -0,0 +1,8 @@
+numb = int(input('Digite um número inteiro: '))
+
+if (numb % 2 == 0):
+    print(f'O número {numb} é par')
+elif (numb % 2 != 0):
+    print(f'O número {numb} é impar')
+else:
+    print('Número Invalido!')
