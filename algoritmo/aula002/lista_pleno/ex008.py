0ecd9647f20169208992e778043cec2b4319504d
Gabriel Iuri <141740769+gabrielyur1@users.noreply.github.com>
2023-12-04 20:59:51 -0300

    Update ex008.py

--- algoritmo/aula002/lista_pleno/ex008.py
+++ algoritmo/aula002/lista_pleno/ex008.py
@@ -3,3 +3,8 @@ data_input = input("Digite uma data no formato dd/mm/aaaa: ")
 # Divide a string da data
 data_parts = data_input.split('/')
 dia, mes, ano = map(int, data_parts)
+
+if (dia >= 1 and dia <= 31) and (mes >= 1 and mes <= 12) and (ano <= 2021):
+    print('Data válida!')
+else:
+    print('Data inválida!')
