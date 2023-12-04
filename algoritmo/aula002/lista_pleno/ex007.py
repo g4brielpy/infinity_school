c67abdb623c05ff66b80fd85c436cc1ec504132e
gabrielyur1 <gabrielsantos031bh@gmail.com>
2023-12-04 14:39:25 -0300

    Update ex007.py

--- algoritmo/aula002/lista_pleno/ex007.py
+++ algoritmo/aula002/lista_pleno/ex007.py
@@ -1,11 +1,20 @@
-d1 = 'Domingo'
-d2 = 'Segunda'
-d3 = 'Terça'
-d4 = 'Quarta'
-d5 = 'Quinta'
-d6 = 'Sexta'
-d7 = 'Sabado'
-
 print('Dias da semana')
-
 dia = int(input('Digite um número: '))
+
+match dia:
+    case 1:
+        print('domingo')
+    case 2:
+        print('Segunda')
+    case 3:
+        print('Terça')
+    case 4:
+        print('Quarta')
+    case 5:
+        print('Quinta')
+    case 6:
+        print('Sexta')
+    case 7:
+        print('Sábado')
+    case _:
+        print('Valor Invalido!')
