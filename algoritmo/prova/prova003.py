003bac5e6b6695293ee82b1a7026c6ef5e08385d
gabrielyur1 <gabrielsantos031bh@gmail.com>
2023-12-06 13:34:37 -0300

    Update prova003.py

--- algoritmo/prova/prova003.py
+++ algoritmo/prova/prova003.py
@@ -1,6 +1,14 @@
 # número da tabuáda
-numb = int(input('Digite o número que deseje ver a tabuada: \n'))
+numb = int(input('\nDigite o número que deseje ver a tabuada de 1 até 10: '))
+print('-- '*5)
 
 # estrutura da tabuada
-for i in range(1, 11):
-    print(f'{numb} X {i} = yy')
+if (numb >= 1) and (numb <= 10):
+    for i in range(1, 11):
+        soma = (numb * i)
+        print(f'{numb} X {i} = {soma}')
+    else:
+        print('-- '*5)
+        print(f'Tabuada do número {numb}\n')
+else:
+    print('Valor inválido!')
