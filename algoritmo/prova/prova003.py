9ce99871b95e3bddf93f99be000133a144552057
Gabriel Iuri <141740769+gabrielyur1@users.noreply.github.com>
2023-12-06 20:46:39 -0300

    Update prova003.py

--- algoritmo/prova/prova003.py
+++ algoritmo/prova/prova003.py
@@ -1,12 +1,14 @@
-# número da tabuáda
+# número da tabuáda WHILE
 numb = int(input('\nDigite o número que deseje ver a tabuada de 1 até 10: '))
+i = int(1)
 print('-- '*5)
 
 # estrutura da tabuada
 if (numb >= 1) and (numb <= 10):
-    for i in range(1, 11):
+    while (i <= 10):
         soma = (numb * i)
         print(f'{numb} X {i} = {soma}')
+        i += 1
     else:
         print('-- '*5)
         print(f'Tabuada do número {numb}\n')
