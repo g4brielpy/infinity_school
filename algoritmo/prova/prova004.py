c3af694bf6c1ecec17b866cfb2a17c0868b91a79
Gabriel Iuri <141740769+gabrielyur1@users.noreply.github.com>
2023-12-06 20:41:53 -0300

    Create prova004.py

---
+++ algoritmo/prova/prova004.py
@@ -0,0 +1,14 @@
+# número da tabuáda FOR
+numb = int(input('\nDigite o número que deseje ver a tabuada de 1 até 10: '))
+print('-- '*5)
+
+# estrutura da tabuada
+if (numb >= 1) and (numb <= 10):
+    for i in range(1, 11):
+        soma = (numb * i)
+        print(f'{numb} X {i} = {soma}')
+    else:
+        print('-- '*5)
+        print(f'Tabuada do número {numb}\n')
+else:
+    print('Valor inválido!')
