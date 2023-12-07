ad342d336980ff445f26f7b6266c33aaeb7ce4ad
gabrielyur1 <gabrielsantos031bh@gmail.com>
2023-12-07 14:48:59 -0300

    Update ex002.py

--- algoritmo/lista_python/estrutura_repeticao/ex002.py
+++ algoritmo/lista_python/estrutura_repeticao/ex002.py
@@ -1,8 +1,12 @@
 while True:
     nome = str(input('Digite seu nome de usuário: '))
     senha = str(input('Digite sua senha: '))
+    login = {
+        'Nome': nome,
+        'Senha': senha,
+    }
 
-    if (nome == senha):
+    if (login['Nome'] == login['Senha']):
         print('ERRO: Senha não pode ser igual ao nome! \n')
     else:
         print('Acesso')
