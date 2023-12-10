29908709b6dad8b40688ad80d8c7c98c1c2f1ad1
gabrielyur1 <141740769+gabrielyur1@users.noreply.github.com>
2023-12-10 12:27:28 -0300

    Tentativas igual a 3 chances

--- algoritmo/aula004/ex004.py
+++ algoritmo/aula004/ex004.py
@@ -1,5 +1,6 @@
 user = 'gabriel'
 senha = '123'
+tentativa = 1
 
 user_input = str('')
 senha_input = str('')
@@ -8,6 +9,11 @@ login = False
 while (login != True):
     user_input = str(input('\nDigite seu user: '))
     senha_input = str(input('Digite sua senha: '))
+
+    if (tentativa == 3):
+        print('\nAcabou a chance!')
+        break
+    tentativa += 1
 
     if (user_input == user) and (senha_input == senha):
         print('\nEntrou')
