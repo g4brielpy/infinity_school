5501183de41a5e5e2d6d8187e80e6a15497c4383
gabrielyur1 <gabrielsantos031bh@gmail.com>
2023-12-13 10:44:04 -0300

    Create prova008.py

---
+++ algoritmo/prova/prova008.py
@@ -0,0 +1,21 @@
+x = 27
+tentativas = 1
+
+print('\nADIVINHE O NÚMERO\n')
+
+while True:
+    numb_input = int(input('Digite o número: '))
+    if (numb_input != x):
+        print('Valor errado!')
+        print('Tente novamente')
+        tentativas += 1
+        if (numb_input > x):
+            print('Número maior que o valor a ser adivinhado\n')
+        else:
+            print('Número menor que o valor a ser adivinhado\n')
+
+    else:
+        break
+
+print(f'\nParabéns, o valor erá {x}')
+print(f'Número de tentativa: {tentativas}')
