de242b0329e333d66da796b76acf7d20abc3f48f
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-15 19:10:28 -0300

    Create prova004.py

---
+++ python/prova/prova004.py
@@ -0,0 +1,9 @@
+'''
+Desenvolva um programa em Python que permita ao usuário digitar várias notas. 
+
+Em seguida, crie uma função chamada "calcular_media" que irá receber as notas digitadas e calcular a média do aluno.
+
+Também crie uma função chamada "verificar_situacao" que, com base na média calculada, irá exibir a situação do aluno: "Reprovado" se a média for menor que 7, 
+"Aprovado" se a média for maior ou igual a 7, e
+"Parabéns, sua média é 10" se a média for igual a 10.
+'''
