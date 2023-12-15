0a16efc97b537879d0640c15bffbe9776afcb8a5
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-15 19:36:14 -0300

    Estrutura básica da aplicação

--- python/prova/prova004.py
+++ python/prova/prova004.py
@@ -3,7 +3,43 @@ Desenvolva um programa em Python que permita ao usuário digitar várias notas.
 
 Em seguida, crie uma função chamada "calcular_media" que irá receber as notas digitadas e calcular a média do aluno.
 
-Também crie uma função chamada "verificar_situacao" que, com base na média calculada, irá exibir a situação do aluno: "Reprovado" se a média for menor que 7, 
+Também crie uma função chamada "verificar_situacao" que, com base na média calculada, irá exibir a situação do aluno: 
+
+"Reprovado" se a média for menor que 7, 
 "Aprovado" se a média for maior ou igual a 7, e
 "Parabéns, sua média é 10" se a média for igual a 10.
 '''
+# variáveis
+lista_notas = list()
+media = int()
+quantidad_notas = 1
+
+# coletar notas
+while True:
+
+    notas = str(input(f'\nDigite sua {quantidad_notas}° Nota: '))
+    # verificar se a nota é valida e atribuir o valor da lista
+    if notas.isdigit():
+        notas = int(notas)
+
+        lista_notas.append(notas)
+        quantidad_notas += 1
+
+    else:
+        print('Valor inválido! Informe novamente')
+        continue
+
+    while True:
+    # verificar se o user vai informar outra nota
+        mais_notas = str(input('\nDeseja informar outra Nota [S / N]: ').upper())
+        if (mais_notas == 'S') or (mais_notas == 'N'):
+            break
+
+        else:
+            print('valor inválido!')
+    if (mais_notas == 'S'):
+        continue
+    else:
+        break
+
+print(lista_notas)
