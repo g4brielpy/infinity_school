1408b99e06804aea6c8d425667613546afc4aed8
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-20 19:44:45 -0300

    Função map() - quadrado

--- python/prova/prova005.py
+++ python/prova/prova005.py
@@ -1,2 +1,22 @@
 # pytthon - super: aula 5 | função II
 
+'''
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Utilizando as funções map(),filter() e reduce(), escreva um código que execute as seguintes operações:
+
+
+Função map() para obter uma nova lista com o quadrado de cada número da lista numeros
+
+Função filter() para obter uma nova lista com números pares da lista numeros
+
+Função reduce()  para obter a soma de todos os números da lista numeros
+
+Qual o resultado obtido após a execução das operações acima?
+'''
+# lista de números
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# função map - quadrado de cada número da lista numeros
+quadrado = list(map(lambda x: x **2, numeros))
+print(quadrado)
+
