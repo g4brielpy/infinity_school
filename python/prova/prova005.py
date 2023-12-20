b94ccc2fe803fe5ab35ac803ba18da537d9919ff
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-20 20:02:37 -0300

    Update prova005.py

--- python/prova/prova005.py
+++ python/prova/prova005.py
@@ -1,18 +1,5 @@
 # pytthon - super: aula 5 | função II
 
-'''
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-Utilizando as funções map(),filter() e reduce(), escreva um código que execute as seguintes operações:
-
-
-Função map() para obter uma nova lista com o quadrado de cada número da lista numeros
-
-Função filter() para obter uma nova lista com números pares da lista numeros
-
-Função reduce()  para obter a soma de todos os números da lista numeros
-
-Qual o resultado obtido após a execução das operações acima?
-'''
 # import de funções
 from functools import reduce
 
