170b056dc6e52e04fadd7b40c93be59b46621ee6
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-22 10:38:30 -0300

    Validações da Função add_Tarefas

--- python/projetos/projeto001.py
+++ python/projetos/projeto001.py
@@ -1,8 +1,32 @@
+# nome, descrição, prioridade e categoria.
+
 # VARIÁVEIS
 Lista_Tarefas = list()
 
 
 # FUNÇÕES TAREFAS
+def add_tarefa():
+    # valida nome
+    while True:
+        nome_input = str(input('Nome da Tarefa: '))
+        
+        if nome_input.isalpha():
+            break
+        else:
+            print('Valor invalido')
+    
+    descricao_input = str(input('Descrição da Tarefa: '))
+    
+    # valida prioridade
+    while True:
+        prioridade_input = str(input('Prioridade [A - alta] [M - média] [B - baixa]: ').upper())
+
+        if (prioridade_input == 'A') or (prioridade_input == 'M') or (prioridade_input == 'B'):
+            break
+        else:
+            print('Valor invalido')
+
+    categoria_input = str(input('Categoria: '))
 
 
 # MENU DE COMANDOS
@@ -15,3 +39,5 @@ print('[4] - Tarefas por Prioridade')
 print('[5] - Exit')
 comando = str(input('COMANDO: '))
 print(' ')
+
+add_tarefa()
