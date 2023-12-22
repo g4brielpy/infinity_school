43a6a47290e4ce0868ad85720966f49fe299e1f4
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-22 10:16:18 -0300

    Menu de comandos

--- python/projetos/projeto001.py
+++ python/projetos/projeto001.py
@@ -1,1 +1,17 @@
-==
+# VARIÁVEIS
+Lista_Tarefas = list()
+
+
+# FUNÇÕES TAREFAS
+
+
+# MENU DE COMANDOS
+print('\n  MENU PRINCIPAL')
+print('~~~~~~~~~~~~~~~~~~')
+print('[1] - Add Tarefa')
+print('[2] - Del Tarefa')
+print('[3] - Lista de Tarefas')
+print('[4] - Tarefas por Prioridade')
+print('[5] - Exit')
+comando = str(input('COMANDO: '))
+print(' ')
