ef4102b2e83c925e9e7c3b212a5d212ec0597627
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-27 14:32:19 -0300

    Menu principal

--- python/prova/prova006/index.py
+++ python/prova/prova006/index.py
@@ -2,3 +2,12 @@ import add_aluno
 import del_aluno
 import lista_alunos
 
+# Menu principal
+print('\n MENU DE ALUNOS')
+print('~ '*8)
+print(' ')
+print('[1] - Adicionar Aluno')
+print('[2] - Remover Aluno')
+print('[3] - Atualizar Aluno')
+print('[4] - Lista de Alunos')
+comando = str(input('COMANDO: '))
