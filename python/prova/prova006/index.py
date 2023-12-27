70381ee5ff2c4b06b07aaa1bfac082d666cd6963
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-27 15:20:30 -0300

    Update index.py

--- python/prova/prova006/index.py
+++ python/prova/prova006/index.py
@@ -1,13 +1,27 @@
-import add_aluno
-import del_aluno
-import lista_alunos
+from add_aluno import AdicionarAluno
 
 # Menu principal
-print('\n MENU DE ALUNOS')
-print('~ '*8)
-print(' ')
-print('[1] - Adicionar Aluno')
-print('[2] - Remover Aluno')
-print('[3] - Atualizar Aluno')
-print('[4] - Lista de Alunos')
-comando = str(input('COMANDO: '))
+while True:
+    print('\n MENU DE ALUNOS')
+    print('~ '*8)
+    print(' ')
+    print('[1] - Adicionar Aluno')
+    print('[2] - Remover Aluno')
+    print('[3] - Atualizar Aluno')
+    print('[4] - Lista de Alunos')
+    comando = str(input('COMANDO: '))
+
+    # estrutura de chamada de módulo
+    match comando:
+        case '1':
+            AdicionarAluno()
+        # case '2':
+            # Função
+        # case '3':
+            # Função
+        # case '4':
+            # Função
+        case '5':
+            break
+        case _:
+            print('Valor inválido')
