302faefe26c73301614713fa1139661081d21e55
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-27 15:21:03 -0300

    Função adicionar alunos validada e concluída.

--- python/prova/prova006/add_aluno.py
+++ python/prova/prova006/add_aluno.py
@@ -1,16 +1,25 @@
 # variáveis
-aluno = dict()
 lista_alunos = list()
+aluno = dict()
 
 # função adicionar alunos
 def AdicionarAluno():
     print('ADICIONAR ALUNO')
     # validar núumero de matrícula
     while True:
+        # entrada da matrícula
         matricula_input = str(input('Matrícula do aluno: '))
         if matricula_input.isdigit():
             break
         else:
-            print('Valor inválido, digite apenas número')
+            print('Valor inválido, digite apenas número\n')
+    # entrada do nome
+    nome_input = str(input('Nome do aluno: '))
 
-    nome_input = str(input('Nome do aluno:'))
+    # adicionando aluno em um dicionário, com matrícula de {chave}
+    aluno = {
+        matricula_input: nome_input,
+    }
+    # adicionando à lista de alunos
+    lista_alunos.append(aluno)
+    print(lista_alunos)
