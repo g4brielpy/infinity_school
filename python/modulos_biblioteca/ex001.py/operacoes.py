f19158363fd9abaedba1a2efd4c40ed812c657a0
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-29 15:38:51 -0300

    Update operacoes.py

--- python/modulos_biblioteca/ex001.py/operacoes.py
+++ python/modulos_biblioteca/ex001.py/operacoes.py
@@ -1,23 +1,23 @@
 def soma():
-    numb_1 = int(input('Digite o 1° número: '))
-    numb_2 = int(input('Digite o 2° número: '))
+    numb_1 = float(input('Digite o 1° número: '))
+    numb_2 = float(input('Digite o 2° número: '))
     r = numb_1 + numb_2
     print(f'A soma de {numb_1} + {numb_2} é {r}')
 
 def subtrair():
-    numb_1 = int(input('Digite o 1° número: '))
-    numb_2 = int(input('Digite o 2° número: '))
+    numb_1 = float(input('Digite o 1° número: '))
+    numb_2 = float(input('Digite o 2° número: '))
     r = numb_1 + numb_2
     print(f'A subtração de {numb_1} - {numb_2} é {r}')
 
 def multiplicacao():
-    numb_1 = int(input('Digite o 1° número: '))
-    numb_2 = int(input('Digite o 2° número: '))
+    numb_1 = float(input('Digite o 1° número: '))
+    numb_2 = float(input('Digite o 2° número: '))
     r = numb_1 + numb_2
     print(f'A multiplicação de {numb_1} x {numb_2} é {r}')
 
 def divisao():
-    numb_1 = int(input('Digite o 1° número: '))
-    numb_2 = int(input('Digite o 2° número: '))
+    numb_1 = float(input('Digite o 1° número: '))
+    numb_2 = float(input('Digite o 2° número: '))
     r = numb_1 + numb_2
     print(f'A divisão de {numb_1} / {numb_2} é {r}')
