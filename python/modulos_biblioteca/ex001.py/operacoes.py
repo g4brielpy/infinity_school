1a1f10f3c207f9ab071408accf784139215c3d8d
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-29 15:43:14 -0300

    Update operacoes.py

--- python/modulos_biblioteca/ex001.py/operacoes.py
+++ python/modulos_biblioteca/ex001.py/operacoes.py
@@ -7,17 +7,17 @@ def soma():
 def subtrair():
     numb_1 = float(input('Digite o 1° número: '))
     numb_2 = float(input('Digite o 2° número: '))
-    r = numb_1 + numb_2
+    r = numb_1 - numb_2
     print(f'A subtração de {numb_1} - {numb_2} é {r}')
 
 def multiplicacao():
     numb_1 = float(input('Digite o 1° número: '))
     numb_2 = float(input('Digite o 2° número: '))
-    r = numb_1 + numb_2
+    r = numb_1 * numb_2
     print(f'A multiplicação de {numb_1} x {numb_2} é {r}')
 
 def divisao():
     numb_1 = float(input('Digite o 1° número: '))
     numb_2 = float(input('Digite o 2° número: '))
-    r = numb_1 + numb_2
+    r = numb_1 / numb_2
     print(f'A divisão de {numb_1} / {numb_2} é {r}')
