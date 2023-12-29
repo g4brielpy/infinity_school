f9db03e7b1988bc7ce16dec352221a5553fddd52
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-29 15:37:04 -0300

    Módulo Multiplicação

--- python/modulos_biblioteca/ex001.py/operacoes.py
+++ python/modulos_biblioteca/ex001.py/operacoes.py
@@ -9,3 +9,9 @@ def subtrair():
     numb_2 = int(input('Digite o 2° número: '))
     r = numb_1 + numb_2
     print(f'A subtração de {numb_1} - {numb_2} é {r}')
+
+def multiplicacao():
+    numb_1 = int(input('Digite o 1° número: '))
+    numb_2 = int(input('Digite o 2° número: '))
+    r = numb_1 + numb_2
+    print(f'A multiplicação de {numb_1} - {numb_2} é {r}')
