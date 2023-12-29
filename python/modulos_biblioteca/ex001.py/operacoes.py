af79929bab50fc927e8be64cfac8cad592ddda53
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-29 15:36:19 -0300

    Módulo subtração

--- python/modulos_biblioteca/ex001.py/operacoes.py
+++ python/modulos_biblioteca/ex001.py/operacoes.py
@@ -4,3 +4,8 @@ def soma():
     r = numb_1 + numb_2
     print(f'A soma de {numb_1} + {numb_2} é {r}')
 
+def subtrair():
+    numb_1 = int(input('Digite o 1° número: '))
+    numb_2 = int(input('Digite o 2° número: '))
+    r = numb_1 + numb_2
+    print(f'A subtração de {numb_1} - {numb_2} é {r}')
