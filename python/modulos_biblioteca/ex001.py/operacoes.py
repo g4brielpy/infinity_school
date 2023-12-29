d618a9e7afd168d0214dc3c8d2939d4a041ffdc1
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-29 15:34:28 -0300

    Módulo soma

---
+++ python/modulos_biblioteca/ex001.py/operacoes.py
@@ -0,0 +1,6 @@
+def soma():
+    numb_1 = int(input('Digite o 1° número: '))
+    numb_2 = int(input('Digite o 2° número: '))
+    r = numb_1 + numb_2
+    print(f'A soma de {numb_1} + {numb_2} é {r}')
+
