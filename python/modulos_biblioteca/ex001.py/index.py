2afb80ebed3537f8d3aa6426abcf19efb5e86d56
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-29 15:29:22 -0300

    Menu pronto

--- python/modulos_biblioteca/ex001.py/index.py
+++ python/modulos_biblioteca/ex001.py/index.py
@@ -1,5 +1,10 @@
 # Calculadora usando módulos
 
 # menu
-print('CALCULADORA')
-print('Qual operação deseja fazer [soma; subtração;]: ')
+print('\nCALCULADORA')
+print('Qual operação deseja fazer')
+print('[1] - Soma')
+print('[2] - Subtrair')
+print('[3] - Multiplicar')
+print('[4] - Dividir')
+comando = str(input('COMANDO: '))
