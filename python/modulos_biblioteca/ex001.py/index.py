eaa2fabb8e3c0bf10131e74f666e99be97c673c3
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2023-12-29 15:41:44 -0300

    Update index.py

--- python/modulos_biblioteca/ex001.py/index.py
+++ python/modulos_biblioteca/ex001.py/index.py
@@ -1,10 +1,26 @@
 # Calculadora usando módulos
+import operacoes
 
 # menu
-print('\nCALCULADORA')
-print('Qual operação deseja fazer')
-print('[1] - Soma')
-print('[2] - Subtrair')
-print('[3] - Multiplicar')
-print('[4] - Dividir')
-comando = str(input('COMANDO: '))
+while True:
+    print('\nCALCULADORA')
+    print('Qual operação deseja fazer')
+    print('[1] - Soma')
+    print('[2] - Subtrair')
+    print('[3] - Multiplicar')
+    print('[4] - Dividir')
+    comando = str(input('COMANDO: '))
+
+    match comando:
+        case '1':
+            operacoes.soma()
+        case '2':
+            operacoes.subtrair()
+        case '3':
+            operacoes.multiplicacao()
+        case '4':
+            operacoes.divisao()
+        case '5':
+            break
+        case _:
+            print('Valor inválido')
