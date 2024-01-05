efcb13a6a86f0d4d3575959275bd796aca561470
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2024-01-05 17:56:00 -0300

    Menu de opções

---
+++ python/prova/prova007/home.py
@@ -0,0 +1,10 @@
+# Menu principal
+while True:
+    print('\nMENU DE PRODUTOS')
+    print('-- '*6)
+
+    print('[1] - Adicionar produtos')
+    print('[2] - Remover produto')
+    print('[3] - Lista de produtos')
+    print('[5] - Atualizar produtos')
+    print('[6] - Encerrar programa')
