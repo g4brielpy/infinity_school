424f9905f72b1ca20b5619c1c988805b98291507
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2024-01-05 19:49:05 -0300

    Função remover produto finalizada

--- python/prova/prova007/del_produtos.py
+++ python/prova/prova007/del_produtos.py
@@ -1,2 +1,26 @@
+"""
+Remover produto: 
+    O usuário pode remover um produto da lista informando o nome do produto que deseja remover. 
+    O programa atualiza automaticamente o valor total de todos os produtos.
+"""
+
+
 def removerProduto(listaProdutos):
-    return
+    # produto a ser removido
+    nomeRevomer_input = str(input('Nome do produto que deseja remover: '))
+
+    listaProdutos_clone = listaProdutos[:]
+
+    for produto in listaProdutos_clone:
+    # produto = dicionário de cada item; contendo todas as informações de um produto
+        
+        if produto['nome'] in nomeRevomer_input:
+            # localizando o produto
+            print(f'Produto removido: {produto["nome"]}')
+
+            listaProdutos.remove(produto)
+            return listaProdutos # Retorna apenas se encontrar o produto a ser removido
+
+    # caso o produto não exista
+    print(f'Produto {nomeRevomer_input} não localizado')
+    return listaProdutos # Retorna a lista original se o produto não for encontrado
