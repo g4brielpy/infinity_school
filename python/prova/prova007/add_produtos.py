fd435cbdd7c839e58c484abce833f5a488017182
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2024-01-05 18:21:24 -0300

    Função adicionar produto

---
+++ python/prova/prova007/add_produtos.py
@@ -0,0 +1,24 @@
+"""
+Adicionar produtos: 
+    O usuário pode adicionar um novo produto à lista informando o NOME, a QUANTIDADE e o VALOR UNITÁRIO do produto. 
+    O programa calcula automaticamente o VALOR TOTAL do produto (quantidade * valor unitário) e atualiza o valor total de todos os produtos.
+"""
+
+
+def adicionar_produto(lista_produtos):
+
+    # cadastro do produto
+    nome_input = str(input('Nome do produto: '))
+    quantidade_input = int(input(f'Quantidade de {nome_input}: '))
+    valorUnitario_input = float(input('Valor Unitário: R$ '))
+
+    valorTotal = (quantidade_input * valorUnitario_input)
+
+    produto = {
+        'nome': nome_input,
+        'quatidade': quantidade_input,
+        'valor unitario': valorUnitario_input,
+        'valor total': valorTotal
+    }
+    lista_produtos.append(produto)
+    print(lista_produtos)
--- python/prova/prova007/home.py
+++ python/prova/prova007/home.py
@@ -1,3 +1,9 @@
+# módulos
+import add_produtos
+
+# variáveis
+lista_produtos = list()
+
 # Menu principal
 while True:
     print('\nMENU DE PRODUTOS')
@@ -8,3 +14,8 @@ while True:
     print('[3] - Lista de produtos')
     print('[5] - Atualizar produtos')
     print('[6] - Encerrar programa')
+    comando =str(input('COMANDO: '))
+
+    match comando:
+        case '1':
+            add_produtos.adicionar_produto(lista_produtos)
