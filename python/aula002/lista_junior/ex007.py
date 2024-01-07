02fe8c60e7d996116feb8bb313d8214e94fc1eae
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-01-07 11:12:56 -0300

    Lista junior - ex007

---
+++ python/aula002/lista_junior/ex007.py
@@ -0,0 +1,12 @@
+# 7)Com a lista do exercício anterior insira um novo argumento na posição 1
+
+# ex. antigo
+lista = []
+lista.append(1)
+lista.append(2)
+lista.append(3)
+print(lista)
+
+# add. novo item na posição 1
+lista.insert(1, 'posicao 1')
+print(lista)
