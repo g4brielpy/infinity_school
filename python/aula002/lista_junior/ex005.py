83f5516fb160a664029e059465c02aeefa4f0bb1
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-01-07 11:03:11 -0300

    Lista junior - ex005

---
+++ python/aula002/lista_junior/ex005.py
@@ -0,0 +1,9 @@
+'''
+5)Crie uma lista e com 3 elementos do tipo string e mude o 2 elemento para a palavra 'uva' mostre a lista antes e depois
+'''
+
+lista = ['a' ,'b', 'c']
+print(lista)
+
+lista[1] = 'uva'
+print(lista)
