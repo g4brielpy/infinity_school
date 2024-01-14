2fecabb4f2fe8163145a88a92e0a562921ae0cd4
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-01-14 11:14:19 -0300

    Lista junior - ex006

---
+++ python/aula003/lista_junior/ex006.py
@@ -0,0 +1,14 @@
+# crie uma variavel que retorne uma lista de chaves do dicionário da questão dois
+
+dados = {
+    'nome': 'Gabriel',
+    'sobrenome': 'Iuri',
+    'idade': 18,
+    'anivesario': '11/12'
+}
+chaves_dados = list()
+
+for i in dados:
+    chaves_dados.append(i)
+
+print(chaves_dados)
