99ac6a3aca99042706301ca423eeb93fffcc3a49
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-01-14 11:34:46 -0300

    Lista junior - ex008

---
+++ python/aula003/lista_junior/ex008.py
@@ -0,0 +1,16 @@
+'''
+Exiba na tela o tamanha do seguinte dicionario:
+
+meu_dicionario = {
+frutas: [maçã, banana, laranja],
+numeros: [1, 2, 3, 4, 5],
+pessoas: [Alice, Bob, Carol]
+}
+'''
+
+meu_dicionario = {
+    'frutas': ['maçã', 'banana', 'laranja'],
+    'numeros': [1, 2, 3, 4, 5],
+    'pessoas': ['Alice', 'Bob', 'Carol']
+}
+print(len(meu_dicionario))
