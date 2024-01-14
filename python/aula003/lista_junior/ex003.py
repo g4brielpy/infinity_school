5c77ff1284d86664836771d51d629bcf50adca20
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-01-14 10:32:19 -0300

    Lista junior - ex003

---
+++ python/aula003/lista_junior/ex003.py
@@ -0,0 +1,10 @@
+# No dicionario do exercicio 2 exiba na tela apenas o valor nome.
+
+dados = {
+    'nome': 'Gabriel',
+    'sobrenome': 'Iuri',
+    'idade': 18,
+    'anivesario': '11/12'
+}
+
+print(dados['nome'])
