eee3fa5a9cf27a815b52c1d8f5ca1aff49062f23
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-01-14 11:41:17 -0300

    Lista junior - ex009,  010

---
+++ python/aula003/lista_junior/ex009.py
@@ -0,0 +1,9 @@
+# No dicionario da questao anterior acesse o elemento “Bob” e exiba na tela.
+
+meu_dicionario = {
+    'frutas': ['maçã', 'banana', 'laranja'],
+    'numeros': [1, 2, 3, 4, 5],
+    'pessoas': ['Alice', 'Bob', 'Carol']
+}
+
+print(meu_dicionario['pessoas'] [1])
---
+++ python/aula003/lista_junior/ex010.py
@@ -0,0 +1,10 @@
+# Troque o elemento “Bob” para “Paulo”
+
+meu_dicionario = {
+    'frutas': ['maçã', 'banana', 'laranja'],
+    'numeros': [1, 2, 3, 4, 5],
+    'pessoas': ['Alice', 'Bob', 'Carol']
+}
+meu_dicionario['pessoas'][1] = 'Paulo'
+
+print(meu_dicionario['pessoas'][1])
