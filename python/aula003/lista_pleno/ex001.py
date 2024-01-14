59ea77a1cb4b5066b8af15ccf3d4e205b0773a72
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-01-14 11:50:46 -0300

    Lista pleno - ex001

---
+++ python/aula003/lista_pleno/ex001.py
@@ -0,0 +1,28 @@
+'''
+1. remova todas as chaves e valores do seguinte dicionario:
+
+sacolao = {
+    'Maçã': 3,
+    'Banana': 6,
+    'Laranja': 4,
+    'Pêra': 5,
+    'Uva': 2,
+    'Abacaxi': 1,
+    'Melancia': 7,
+    'Morango': 8
+}
+'''
+
+sacolao = {
+    'Maçã': 3,
+    'Banana': 6,
+    'Laranja': 4,
+    'Pêra': 5,
+    'Uva': 2,
+    'Abacaxi': 1,
+    'Melancia': 7,
+    'Morango': 8
+}
+sacolao.clear()
+
+print(sacolao)
