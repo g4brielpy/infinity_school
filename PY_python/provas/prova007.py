29ac379ab7805cc32f00a991fff6332e84a25831
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2024-01-16 12:23:29 -0300

    Cadastro de alunos

--- PY_python/provas/prova007.py
+++ PY_python/provas/prova007.py
@@ -5,27 +5,40 @@ Por fim, você deverá utilizar um loop for para imprimir a média de cada aluno
 '''
 
 cont = int(1)
+mais_notas = bool(True)
 aluno = bool(True)
+alunos = dict()
 notas = list()
 
 while aluno:
     print(f'\nAluno {cont}')
     i = int(1)
 
-    while True:
+    while mais_notas:
         # entrada das notas do usuário
         nota = float(input(f'Digite o valor da {i}° nota: '))
         notas.append(nota)
         i += 1
 
         # verificando se o usuário quer continuar
-        continuar = str(input('Deseja inserir mais notas [S/N]: '))
-        # encerrando o loop
-        if continuar.upper() == 'N':
-            break
-        # valor inválido
-        elif continuar.upper() not in ['S', 'N']:
-            print('Valor inválido')
+        while True:
+            continuar = str(input('\nDeseja inserir mais notas [S/N]: '))
+            # encerrando o loop
+            if continuar.upper() == 'N':
+                mais_notas = False
+                break
+            # adicionar mais notas
+            elif continuar.upper() == 'S':
+                break
+            # valor inválido
+            elif continuar.upper() not in ['S', 'N']:
+                print('Valor inválido')
+
+    # adicionar notas na chave do aluno
+    notas_aluno = notas.copy()
+    alunos[f'aluno {cont}'] = notas_aluno
+    notas.clear()
+    print(alunos)
 
     while True:
         aluno = str(input(f'\nDeseja cadastrar outro aluno [S/N]: '))
@@ -33,6 +46,7 @@ while aluno:
         # atualizando a quantidade de alunos
         if aluno.upper() == 'S':
             aluno = True
+            mais_notas = True
             cont += 1
             break
         # encerrando o loop
@@ -42,5 +56,3 @@ while aluno:
         # valor inválido
         else:
             print('Valor inválido')
-
-    print(notas)
