e205e33158ccf7c456dae9d981a583b51cae7649
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2024-01-17 16:51:44 -0300

    Criando menus com Tkinter

--- python/tkinter/teste.py
+++ python/tkinter/teste.py
@@ -11,23 +11,24 @@ def funcao_salvar():
 
 root = tk.Tk()
 root.title("Exemplo de Menu")
+root.geometry("400x300")
 
 menuP = tk.Menu(root)
 
 # Adicionando opções diretamente ao menu
 menuP.add_command(label="Novo", command=lambda: print("Novo"))
 menuP.add_command(label="Fechar", command=root.destroy)
+menuP.add_command(label='Teste', command=lambda: print('Teste'))
 
-# Criando um submenu
+# Criando um submenu     definir onde vai ser exibido o submenu 'menuP'
 submenu_arquivo = tk.Menu(menuP, tearoff=0)
 submenu_arquivo.add_command(label="Abrir", command=funcao_abrir)
 submenu_arquivo.add_command(label="Salvar", command=funcao_salvar)
 
-# Adicionando o submenu ao menu principal
-menuP.add_cascade(label="Arquivo", menu=submenu_arquivo)
+# Adicionando o submenu ao menu principal         definir qual menu deve ser exibido
+menuP.add_cascade(label="Acessar submenu", menu=submenu_arquivo)
 
 # associar o menu criado (menu) à janela principal
 root.config(menu=menuP)
-
 
 root.mainloop()
