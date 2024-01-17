21b6de887825495e010eb2df95788b4cd5fd228a
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2024-01-17 17:28:50 -0300

    Ambiente virtual

---
+++ python/tkinter/projeto_mercado/market_ project.py
@@ -0,0 +1,12 @@
+'''
+O objetivo deste projeto é criar um sistema básico de supermercado utilizando a linguagem de programação Python e a biblioteca Tkinter para a construção da interface gráfica. Este sistema possui funcionalidades simples, que proporcionam um ambiente de aprendizado para iniciantes em programação.
+
+Funcionalidades Principais:
+    . Adicionar Produtos
+    . Remover Produtos
+    . Lista de Produtos
+    . Atualizar Produtos
+
+Interface Gráfica:
+    . Uma interface gráfica será construída utilizando o Tkinter para proporcionar uma experiência amigável ao usuário
+'''
