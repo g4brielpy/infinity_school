3a96760da0903b090f847e7b653de8d24840fe23
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-01-22 12:13:15 -0300

    Create desafio001.py

---
+++ python/aula004/desafios/desafio001.py
@@ -0,0 +1,33 @@
+'''
+Exercício 1: Calculadora Simples
+
+Aluno 1: Crie funções para realizar as operações de soma, subtração, multiplicação e divisão.
+Aluno 2: Crie um menu para permitir ao usuário escolher qual operação realizar.
+'''
+
+
+def soma(x, y):
+    return x + y
+
+
+def subtracao(x, y):
+    return x - y
+
+
+def multiplicacao(x, y):
+    return x * y
+
+
+def divisao(x, y):
+    return x / y
+
+
+menu = '''Calculadora
+
+[1] - Soma
+[2] - Subtração
+[3] - Multiplicação
+[4] - Divisão
+[q] - Sair
+=>
+'''
