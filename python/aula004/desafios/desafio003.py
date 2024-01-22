9289db897345b6f1763e9b5746e7894fbbc2c7e7
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-01-22 14:59:03 -0300

    Create desafio003.py

---
+++ python/aula004/desafios/desafio003.py
@@ -0,0 +1,6 @@
+'''
+Exercício 3: Gerador de Senhas
+
+Aluno 1: Desenvolva uma função que gera senhas aleatórias com base em critérios definidos, como cumprimento e caracteres permitidos.
+Aluno 2: Crie um menu para permitir ao usuário gerar senhas personalizadas.
+'''
