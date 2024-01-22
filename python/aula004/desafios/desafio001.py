17df13083f3e2767aed6049d771cc92eb04e7a5a
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-01-22 12:31:04 -0300

    Update desafio001.py

--- python/aula004/desafios/desafio001.py
+++ python/aula004/desafios/desafio001.py
@@ -22,12 +22,35 @@ def divisao(x, y):
     return x / y
 
 
-menu = '''Calculadora
+MENU = '''Calculadora
 
 [1] - Soma
 [2] - Subtração
 [3] - Multiplicação
 [4] - Divisão
 [q] - Sair
-=>
-'''
+=> '''
+
+while True:
+    comando = input(MENU)
+
+    match comando:
+        case '1':
+            num1 = float(input('Digite o primeiro valor: '))
+            num2 = float(input('Digite o segundo valor: '))
+            print(soma(num1, num2))
+        case '2':
+            num1 = float(input('Digite o primeiro valor: '))
+            num2 = float(input('Digite o segundo valor: '))
+            print(subtracao(num1, num2))
+        case '1':
+            num1 = float(input('Digite o primeiro valor: '))
+            num2 = float(input('Digite o segundo valor: '))
+            print(multiplicacao(num1, num2))
+        case '1':
+            num1 = float(input('Digite o primeiro valor: '))
+            num2 = float(input('Digite o segundo valor: '))
+            print(divisao(num1, num2))
+        case 'q':
+            print('Programa finalizado!')
+            break
