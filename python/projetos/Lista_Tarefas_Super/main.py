4033767c0df02472809cbacf15f39aafc463f0bd
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-01-26 14:03:10 -0300

    Update main.py

---
+++ python/projetos/Lista_Tarefas_Super/main.py
@@ -0,0 +1,15 @@
+'''
+Desenvolver um programa de linha de comando que permite
+aos usuários gerenciar suas tarefas diárias, atribuindo-lhes
+prioridades e categorias. O projeto será organizado em várias
+partes e usará funções, listas, tuplas, dicionários, conjuntos e
+um ambiente virtual. 
+'''
+
+MENU = '''MENU TAREFAS
+[1] - Criar Nova Tarefa
+[2] - Listar Tarefas
+[3] - Remover Tarefa
+[Q] - Sair
+=> '''
+
