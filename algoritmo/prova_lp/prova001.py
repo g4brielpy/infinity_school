f387c3507a73d31452117840d7a04618dda7b5c2
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-02-01 11:18:19 -0300

    Create prova001.py

---
+++ algoritmo/prova_lp/prova001.py
@@ -0,0 +1,22 @@
+'''
+Crie um exemplo de cada uma das principais tipagens de variáveis.
+
+Crie uma solicitação de dados ao usuário.
+
+Imprima a solicitação anterior na tela com uma mensagem personalizada.
+'''
+
+# principais tipagens: int, float, str, bool
+texto = 'Olá, Mundo!'
+numero_inteiro = 10
+numero_real = 5.25
+booleano = True
+
+
+# solicitação de dados ao usuário: input
+nome = str(input('Digite seu nome: '))
+idade = int(input('Digite sua idade: '))
+
+# Imprima a solicitação: print
+print(f'\nSeu nome é {nome}')
+print(f'Sua idade é {idade}')
