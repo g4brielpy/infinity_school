426b8ec1c64ea78c0876d0055e2906442c12a91c
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2024-02-03 17:02:46 -0300

    Prova POO - aplicação de polimorfismo

---
+++ PY_python/poo/prova002/main.py
@@ -0,0 +1,7 @@
+'''
+Desenvolva um exemplo de aplicação de polimorfismo utilizando super classe.
+
+Crie uma classe pai que defina um método genérico, e em seguida crie duas ou mais classes filhas que herdem essa super classe
+
+e sobrescrevam o método de acordo com suas necessidades.
+'''
