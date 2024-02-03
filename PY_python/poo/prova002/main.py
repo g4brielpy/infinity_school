3f07c9f0c3c7f338a0ee9b67aee3ae81fb2be2d2
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2024-02-03 17:19:48 -0300

    Update main.py

--- PY_python/poo/prova002/main.py
+++ PY_python/poo/prova002/main.py
@@ -5,3 +5,40 @@ Crie uma classe pai que defina um método genérico, e em seguida crie duas ou m
 
 e sobrescrevam o método de acordo com suas necessidades.
 '''
+
+
+class Animal:
+    def __init__(self, nome) -> None:
+        self.nome = nome
+
+    def fazer_som(self) -> None:
+        pass
+
+
+class Cachorro(Animal):
+    def __init__(self, nome: str, raca: str) -> None:
+        super().__init__(nome)
+        self.raca = raca
+
+    def fazer_som(self) -> str:
+        return "Au au!"
+
+
+class Gato(Animal):
+    def __init__(self, nome: str, raca: str) -> None:
+        super().__init__(nome)
+        self.raca = raca
+
+    def fazer_som(self) -> str:
+        return "Miau!"
+
+
+
+cachorro = Cachorro('Bob', 'Pinscher')
+gato = Gato('Rob', 'Persa')
+
+print(cachorro.nome)
+print(cachorro.fazer_som())
+
+print(gato.nome)
+print(gato.fazer_som())
