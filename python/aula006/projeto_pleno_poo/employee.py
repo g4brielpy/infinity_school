a5ff11c1e6e4f13b9843b2f0325a63868c23ea85
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-02-06 15:04:43 -0300

    Update employee.py

--- python/aula006/projeto_pleno_poo/employee.py
+++ python/aula006/projeto_pleno_poo/employee.py
@@ -12,17 +12,37 @@ class Funcionario:
 
         # pegar todas as iniciais dos sobrenomes
         user += ''.join([i[0] for i in nomes[1:]])
-        user = user.lower()
 
+        user = user.lower()
         return user
 
-    def format_data(self):
-        pass
+    def format_data(self) -> str:
+        # lista com todos os meses
+        lista_meses = (
+            'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
+            'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro',
+        )
+
+        # separando a data por d/m/a
+        dd, mm, aa = self.data_nascimento.split('/')
+
+        # verificando se o mes contem número 0
+        if '0' in mm:
+            mm = mm.replace('0', '')
+            mes = lista_meses[int(mm) - 1]
+        else:
+            mes = lista_meses[int(mm) - 1]
+
+        # formando a data conforme foi solicitado
+        data_formatada = f'{dd} de {mes} {aa}'
+        return f'{self.nome} nasceu no dia {data_formatada}'
+
+        # retirando o '0'
 
     def format_endereço(self):
         pass
 
-
+# entrada de dados padrão
 dados = {
     'nome': 'Gabriel Iuri Dos Santos',
     'data_nascimento': '11/12/2005',
@@ -30,5 +50,13 @@ dados = {
     'endereco': 'Rua Nossa Senhora, 190, Barreiro, Belo Horizonte'
 }
 
-biel = Funcionario(dados)
-print(biel.cria_user())
+# teste
+try:
+    biel = Funcionario(dados)
+
+    print(biel.cria_user())
+    print(biel.format_data())
+    
+except Exception as e:
+    print(f'Valores inválidos. Exceção: {e}')
+
