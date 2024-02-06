9c6e0b0f3348b1bee23d8e747a047f5a5d40596d
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-02-06 11:28:02 -0300

    Create main.py

---
+++ python/poo/provas/prova002/main.py
@@ -0,0 +1,11 @@
+'''
+Crie um código em Python que contenha as seguintes classes:
+
+A classe principal será chamada de "Material" e terá um construtor que recebe dois parâmetros: "titulo" e "autor_ou_editora". Essa classe também terá um método chamado "exibir_informacoes" que imprimirá na saída o título e o autor/editora do material.
+
+A classe "Livro" será uma subclasse da classe "Material" e terá um construtor que recebe três parâmetros: "titulo", "autor_ou_editora" e "genero". Essa classe também terá um método "exibir_informacoes" que chamará o método correspondente na classe "Material" e imprimirá o gênero do livro.
+
+A classe "Revista" será outra subclasse da classe "Material" e terá um construtor que recebe três parâmetros: "titulo", "autor_ou_editora" e "edicao". Essa classe também terá um método "exibir_informacoes" que chamará o método correspondente na classe "Material" e imprimirá a edição da revista.
+
+Crie instâncias das classes "Livro" e "Revista" com informações específicas e chame o método "exibir_informacoes" para mostrar os detalhes de cada material.
+'''
