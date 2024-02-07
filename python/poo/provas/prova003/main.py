0af976f6899d2cf60d314e1bf6a91bd345e68892
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-02-07 13:24:36 -0300

    Update main.py

---
+++ python/poo/provas/prova003/main.py
@@ -0,0 +1,28 @@
+'''
+Classe Bomba de Combustível:
+Faça um programa completo utilizando classes e métodos que:
+
+Possua uma classe chamada bombaCombustível, com no mínimo esses ATRIBUTOS:
+    i.TipoCombustivel.
+    ii.ValorLitro
+    iii.QuantidadeCombustivel
+
+Possua no mínimo esses MÉTODOS:
+
+    1.abastecerPorValor( )
+        método onde é informado o valor a ser abastecido e mostra a quantidade de litros que foi colocada no veículo
+
+    2.abastecerPorLitro( )
+        método onde é informado a quantidade em litros de combustível e mostra o valor a ser pago pelo cliente.
+
+    3. alterarValor( )
+        altera o valor do litro do combustível.
+
+    4. alterarCombustivel( )
+        altera o tipo do combustível.
+
+    5.  alterarQuantidadeCombustivel( )
+        altera a quantidade de combustível restante na bomba.
+
+OBS: Sempre que acontecer um abastecimento é necessário atualizar a quantidade de combustível total na bomba
+'''
