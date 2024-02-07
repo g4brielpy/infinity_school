89247ba3b36a4708a7329610d4ff6ac87b63314c
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-02-07 14:06:36 -0300

    Update main.py

--- python/poo/provas/prova003/main.py
+++ python/poo/provas/prova003/main.py
@@ -1,11 +1,11 @@
 '''
-Classe Bomba de Combustível:
+Classe BombaCombustível:
 Faça um programa completo utilizando classes e métodos que:
 
 Possua uma classe chamada bombaCombustível, com no mínimo esses ATRIBUTOS:
-    i.TipoCombustivel.
-    ii.ValorLitro
-    iii.QuantidadeCombustivel
+    i.TipoCombustivel. OK
+    ii.ValorLitro. OK
+    iii.QuantidadeCombustivel. OK
 
 Possua no mínimo esses MÉTODOS:
 
@@ -15,6 +15,8 @@ Possua no mínimo esses MÉTODOS:
     2.abastecerPorLitro( )
         método onde é informado a quantidade em litros de combustível e mostra o valor a ser pago pelo cliente.
 
+    OBS: Sempre que acontecer um abastecimento é necessário atualizar a quantidade de combustível total na bomba
+
     3. alterarValor( )
         altera o valor do litro do combustível.
 
@@ -23,6 +25,29 @@ Possua no mínimo esses MÉTODOS:
 
     5.  alterarQuantidadeCombustivel( )
         altera a quantidade de combustível restante na bomba.
-
-OBS: Sempre que acontecer um abastecimento é necessário atualizar a quantidade de combustível total na bomba
 '''
+
+
+class BombaCombustível:
+    def __init__(self, tipo_combustivel: str, valor_litro: float, quantidade_combustivel: float) -> None:
+        if tipo_combustivel and valor_litro and quantidade_combustivel:
+            self.combustivel = str(tipo_combustivel)
+            self.valor_litro = float(valor_litro)
+            self.quantidade = float(quantidade_combustivel)
+        else:
+            raise Exception('Dados não fornecidos.')
+
+    def abastecerPorValor(self, valor_abastecer: float):
+        litros_abastecer = valor_abastecer / self.valor_litro
+
+        if self.quantidade >= litros_abastecer:
+            self.quantidade -= litros_abastecer
+            return f'Valor a ser abastecido: {valor_abastecer}  \nQuantidade de litros: {litros_abastecer}'
+        else:
+            return f'Quantidade de litros indisponível'
+
+        # if self.quantidade >= litro_abastecer:
+        #     custo = litro_abastecer * self.valor_litro
+
+        #     self.quantidade -= litro_abastecer
+        #     return f''
