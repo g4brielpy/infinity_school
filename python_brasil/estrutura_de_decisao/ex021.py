5ce7457d7a05e56c9da985f0fa20a04e8bca69a7
g4brielpy <141740769+g4brielpy@users.noreply.github.com>
2024-02-07 21:57:59 -0300

    Update ex021.py

--- python_brasil/estrutura_de_decisao/ex021.py
+++ python_brasil/estrutura_de_decisao/ex021.py
@@ -8,30 +8,91 @@ OBS:
     As notas disponíveis serão as de 2, 5, 10, 50 e 100 reais. O valor mínimo é de 10 reais e o máximo de 600 reais. O programa não deve se preocupar com a quantidade de notas existentes na máquina.
 '''
 
-valor = float(input("Digite um valor: "))
+'''controle = True
 
-if valor >= 10 and valor <= 600:
-    notas_100 = valor // 100
-    valor = valor % 100
+while controle:
+    valor = float(input("Digite um valor: "))
 
-    notas_50 = valor // 50
-    valor = valor % 50
+    if valor >= 10 and valor <= 600:
+        notas_100 = valor // 100
+        valor = valor % 100
 
-    notas_10 = valor // 10
-    valor = valor % 10
+        notas_50 = valor // 50
+        valor = valor % 50
 
-    notas_5 = valor // 5
-    valor = valor % 5
+        notas_10 = valor // 10
+        valor = valor % 10
 
-    notas_2 = valor // 2
-    print(
-    f"""
-    Total de Notas de R$ 100: {notas_100*100}
-    Total de Notas de R$ 50: {notas_50*50}
-    Total de Notas de R$ 10: {notas_10*10}
-    Total de Notas de R$ 5: {notas_5*5}
-    Total de Notas de R$ 2: {notas_2*2}
-    """
-    )
-else:
-    print("Valor inválido. Tente novamente!")
+        notas_5 = valor // 5
+        valor = valor % 5
+
+        notas_2 = valor // 2
+        valor = valor % 2
+
+        notas_1 = valor // 1
+        print(
+            f"""
+        Total de Notas de R$ 100: {notas_100*100}
+        Total de Notas de R$ 50: {notas_50*50}
+        Total de Notas de R$ 10: {notas_10*10}
+        Total de Notas de R$ 5: {notas_5*5}
+        Total de Notas de R$ 2: {notas_2*2}
+        Total de Notas de R$ 1: {notas_1*1}
+        """
+        )
+    else:
+        print("Valor inválido. Tente novamente!")'''
+        
+
+from os import system
+
+controle = True
+MENU = '''
+CAIXA ELETRÔNICO - BANCO NACIONAL
+[1] - Sacar
+[2] - Sair
+=> '''
+
+while controle:
+    opcao = input(MENU)
+
+    match opcao:
+        case '1':
+            valor = float(input("Digite um valor: "))
+
+            if valor >= 10 and valor <= 600:
+                notas_100 = valor // 100
+                valor = valor % 100
+
+                notas_50 = valor // 50
+                valor = valor % 50
+
+                notas_10 = valor // 10
+                valor = valor % 10
+
+                notas_5 = valor // 5
+                valor = valor % 5
+
+                notas_2 = valor // 2
+                valor = valor % 2
+
+                notas_1 = valor // 1
+                print(
+                    f"""
+                Total de Notas de R$ 100: {notas_100*100}
+                Total de Notas de R$ 50: {notas_50*50}
+                Total de Notas de R$ 10: {notas_10*10}
+                Total de Notas de R$ 5: {notas_5*5}
+                Total de Notas de R$ 2: {notas_2*2}
+                Total de Notas de R$ 1: {notas_1*1}
+                """
+                )
+            else:
+                print("Valor inválido. Tente novamente!")
+                
+            input("Aperte ENTER para continuar...")
+            system("cls")
+        case '2':
+            controle = False
+        case _:
+            print('Opção inválida!')
