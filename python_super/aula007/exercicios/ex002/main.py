e0775481c641dd840ca4b323a1e3c2c064b3a8e6
g4brielpy <141740769+g4brielpy@users.noreply.github.com>
2024-02-18 12:58:32 -0300

    ex002

---
+++ python_super/aula007/exercicios/ex002/main.py
@@ -0,0 +1,9 @@
+'''
+2)Calculadora Simples: Crie um programa que permita ao usuário realizar operações matemáticas complexa (raiz quadrada, exponencial) importando o módulo math.
+'''
+import math
+
+print('CALCULADORA DE RAIZ QUADRADA')
+valor = float(input('Digite um valor: '))
+valor = math.sqrt(valor)
+print(valor)
