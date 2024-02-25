178cbf89d8353d2f9182fedc01ac70e6f8b6854f
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-02-25 12:56:43 -0300

    ex007

---
+++ python_brasil/exercicios_string/ex007.py
@@ -0,0 +1,22 @@
+'''
+Conta espaços e vogais. Dado uma string com uma frase informada pelo usuário (incluindo espaços em branco), conte:
+
+quantos espaços em branco existem na frase.
+quantas vezes aparecem as vogais a, e, i, o, u.
+'''
+
+vogais = ['a', 'e', 'i', 'o', 'u']
+qtd_vogais = 0
+qtd_espacos = 0
+
+frase = input('Digite uma frase: ')
+
+for letra in frase:
+    if letra.lower() in ['a', 'e', 'i', 'o', 'u']:
+        qtd_vogais += 1
+    elif letra == ' ':
+        qtd_espacos += 1
+    
+print(f'\nQuantidade de vogais: {qtd_vogais}')
+print(f'Quantidade de espaços: {qtd_espacos}')
+        
