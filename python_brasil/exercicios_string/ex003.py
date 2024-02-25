a666974357f281375fcb278a33d5236e570171df
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-02-25 12:30:23 -0300

    ex003

---
+++ python_brasil/exercicios_string/ex003.py
@@ -0,0 +1,14 @@
+'''
+Nome na vertical. Faça um programa que solicite o nome do usuário e imprima-o na vertical.
+    F
+    U
+    L
+    A
+    N
+    O
+'''
+
+nome = input('Digite seu nome: ')
+
+for letra in nome:
+    print(letra)
