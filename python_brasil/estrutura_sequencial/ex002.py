fd665ec8ca26d12571b5047fc601140a1c4f656d
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-03-02 14:51:51 -0300

    Ex002

--- python_brasil/estrutura_sequencial/ex002.py
+++ python_brasil/estrutura_sequencial/ex002.py
@@ -1,1 +1,14 @@
-# dadsa
+'''
+Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+'''
+
+while True:
+    try:
+        numero: int = int(input('\nDigite um número: '))
+    except ValueError:
+        print('Valor inválido! Digite novamente.')
+    except Exception:
+        print(f'Erro! Digite novamente.')
+    else:
+        print(f'O número informado é: {numero}')
+        break
