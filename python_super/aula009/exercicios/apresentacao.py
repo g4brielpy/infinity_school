e90a0f0535ddc1d63aa52203424a8238d5de9a78
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-03-03 11:51:34 -0300

    Ex de apresentação

--- python_super/aula009/exercicios/apresentacao.py
+++ python_super/aula009/exercicios/apresentacao.py
@@ -4,9 +4,10 @@ root = tk.Tk()
 root.title('Apresentação')
 root.geometry('350x400')
 
-nome = tk.Label(root, text='Meu nome é Gabriel Iuri', font=('Arial', 20))
+nome = tk.Label(root, text='Meu nome é Gabriel Iuri',
+                font=('Arial', 20), foreground='red')
 idade = tk.Label(root, text='Tenho 18 anos', font=('Arial', 16))
-hobby = tk.Label(root, text='Jogar FPS', font=('Arial', 16))
+hobby = tk.Label(root, text='Meu hobby é Jogar FPS', font=('Arial', 16))
 
 nome.pack(pady=(20, 15))
 idade.pack()
