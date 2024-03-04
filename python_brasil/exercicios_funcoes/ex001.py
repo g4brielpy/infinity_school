52c5494fad2bd0e53148f59622917ffd1a672421
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-03-04 10:39:04 -0300

    Remover Exercícios do Python Brasil

--- python_brasil/exercicios_funcoes/ex001.py
+++
@@ -1,5 +0,0 @@
-def imprimir(n: int):
-    for i in range(n+1):
-        print(f'{i} ' * i)
-        
-imprimir(5)
--- python_brasil/exercicios_funcoes/ex003.py
+++
@@ -1,9 +0,0 @@
-'''
-Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
-'''
-
-def somar(*valores):
-    valores = list(valores)
-    return sum(valores)
-
-print(somar(1, 2, 3))
