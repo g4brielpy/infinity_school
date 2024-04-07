bf32c191bab11f9d31dcdfb47134c2d628c74a6a
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-04-07 13:33:34 -0300

    Class COnta

---
+++ python_super/aula012/conta.py
@@ -0,0 +1,15 @@
+# Ex02. Faça uma classe generica chamada "Conta" que terá as seguintes propriedades:
+# - titular
+# - saldo
+# - numero
+# e os metódos depositar, sacar e transferir
+
+# Você deve configurar tambés os seguintes tipos de conta
+# 1. "ContaCorrente"
+# - Terá o atributo cheque_especial
+# O metódo sacar / transferir deve considerar o cheque especial
+
+# 2. "ContaPoupanca"
+# - Deve ter o atributo "juros_poupanca", 
+# e também um metódo proprio chamado "corrigir_saldo", que vai adicionar o valor do juros ao saldo.
+
