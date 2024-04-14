6a6060e0d3595df1d3e65e55c618dab72a04dae3
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-04-14 11:26:08 -0300

    Class Conta generica

--- python_super/aula012/conta.py
+++ python_super/aula012/conta.py
@@ -10,6 +10,33 @@
 # O metódo sacar / transferir deve considerar o cheque especial
 
 # 2. "ContaPoupanca"
-# - Deve ter o atributo "juros_poupanca", 
+# - Deve ter o atributo "juros_poupanca",
 # e também um metódo proprio chamado "corrigir_saldo", que vai adicionar o valor do juros ao saldo.
 
+from random import randint
+
+
+class Conta:
+    def __init__(self, titular: str, saldo: float, numero: str = "") -> None:
+        self.titular = titular
+        self.saldo = saldo
+        self.numero = "".join([str(randint(0, 9)) for i in range(5)])
+
+    def __repr__(self) -> str:
+        return f"<Conta - {self.titular}/ N{self.numero}/ R${self.saldo}>"
+
+    def depositar(self, valor: float):
+        if valor < 0 and not (isinstance(valor, (float, int))):
+            raise ValueError("O valor do deposito é inválido!")
+
+        self.saldo += valor
+
+    def sacar(self, valor: float):
+        if valor < 0 and not (isinstance(valor, (float, int))):
+            raise ValueError("O valor do deposito é inválido!")
+
+        self.saldo -= valor
+
+    def transferir(self, valor: float, conta: "Conta"):
+        self.sacar(valor)
+        conta.depositar(valor)
