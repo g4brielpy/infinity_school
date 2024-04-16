ec7ec4b752c363998eb896d895193e97d4ec174b
Gabriel Iuri <141740769+g4brielpy@users.noreply.github.com>
2024-04-16 14:35:28 -0300

    Função excluirTarefa() implementada

--- java_script/projetos/lista_de_tarefas/src/scripts/main.js
+++ java_script/projetos/lista_de_tarefas/src/scripts/main.js
@@ -8,44 +8,50 @@ const sectionTarefas = document.querySelector("#listaDeTarefas");
 // iniciando a lista de tarefas como um array;
 let listaTarefas = [];
 
+// função para exibir as tárefas válidas no HTML
+const exibirTarefas = () => {
+  // limpando as tarefas para não duplicar
+  sectionTarefas.innerHTML = "";
+
+  // iterando sobre a lista e exibindo todas as tarefas
+  listaTarefas.forEach((tarefa, index) => {
+    const containerTarefa = document.createElement("li");
+
+    const buttonLixo = document.createElement("button");
+    buttonLixo.setAttribute("class", "buttonLixo");
+    buttonLixo.setAttribute("data-tarefa-id", index);
+
+    const imgLixo = document.createElement("img");
+    imgLixo.setAttribute("src", "src/image/lixo.png");
+    imgLixo.setAttribute("alt", "Lixo");
+
+    buttonLixo.appendChild(imgLixo);
+
+    const estruturaHTML = tarefa;
+
+    // set os atributos necessários, e add id único a cada tarefa
+    containerTarefa.setAttribute("id", `tarefa_${index}`);
+    containerTarefa.setAttribute("class", "boxTarefa");
+
+    // definindo o conteúdo e adicionando a lista de tarefas
+    containerTarefa.innerHTML = estruturaHTML;
+    containerTarefa.appendChild(buttonLixo);
+    sectionTarefas.appendChild(containerTarefa);
+  });
+
+  // add evento de click para deletar tarefa
+  const listaButtonLixo = document.querySelectorAll(".buttonLixo");
+  listaButtonLixo.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      excluirTarefa(index);
+    });
+  });
+};
+
 // função base para adicionar as tarefas
-const adcionarTarefa = () => {
+const adicionarTarefa = () => {
   // tarefa há ser acresentada
   const tarefa = inputAddTarefa.value.trim();
-
-  // função para exibir as tárefas válidas no HTML
-  const exibirTarefas = () => {
-    // limpando as tarefas para não duplicar
-    sectionTarefas.innerHTML = "";
-
-    // iterando sobre a lista e exibindo todas as tarefas
-    listaTarefas.forEach((tarefa, index) => {
-      const containerTarefa = document.createElement("li");
-
-      const buttonLixo = document.createElement("button");
-      buttonLixo.setAttribute("class", "buttonLixo");
-      buttonLixo.setAttribute("data-tarefa-id", index);
-
-      const imgLixo = document.createElement("img");
-      imgLixo.setAttribute("src", "src/image/lixo.png");
-      imgLixo.setAttribute("alt", "Lixo");
-
-      buttonLixo.appendChild(imgLixo);
-
-      const estruturaHTML = `
-        ${tarefa}
-      `;
-
-      // set os atributos necessários, e add id único a cada tarefa
-      containerTarefa.setAttribute("id", `tarefa_${index}`);
-      containerTarefa.setAttribute("class", "boxTarefa");
-
-      // definindo o conteúdo e adicionando a lista de tarefas
-      containerTarefa.innerHTML = estruturaHTML;
-      containerTarefa.appendChild(buttonLixo);
-      sectionTarefas.appendChild(containerTarefa);
-    });
-  };
 
   // função para verifica se a tarefa é válida
   const verificarTarefaValida = () => {
@@ -74,13 +80,14 @@ const adcionarTarefa = () => {
 };
 
 const excluirTarefa = (id) => {
-  alert("Função excluir");
+  listaTarefas.splice(id, 1);
+  exibirTarefas();
 };
 
 // adicionar um observador de 'clicks' no elemento 'button' e 'lixeira'
 buttonAdd.addEventListener("click", () => {
   // adicionar tarefa
-  adcionarTarefa();
+  adicionarTarefa();
   // limpar o valor do input
   inputAddTarefa.value = "";
 });
